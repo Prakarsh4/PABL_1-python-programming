398156164abcfd6a472a4a1cdbda01203c1f1129
Prakarsh4 <Prakarshkumar819@gmail.com>
2026-02-25 09:47:16 +0530

    another method

--- duplicate_number.py
+++ duplicate_number.py
@@ -1,19 +1,34 @@
-class Solution:
+"""class Solution:
     def findDuplicate(self, nums):
         slow = nums[0]
         fast = nums[0]
-
-        # Detect cycle
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
             if slow == fast:
                 break
-
-        # Find entrance of cycle
         slow = nums[0]
         while slow != fast:
             slow = nums[slow]
             fast = nums[fast]
+        return slow"""
+n = int(input())
+arr = list(map(int, input().split()))
 
-        return slow
+freq = {}
+
+for num in arr:
+    if num in freq:
+        freq[num] += 1
+    else:
+        freq[num] = 1
+
+max_count = 0
+result = arr[0]
+
+for num in freq:
+    if freq[num] > max_count:
+        max_count = freq[num]
+        result = num
+
+print(result)
